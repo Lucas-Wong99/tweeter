7455c4175e24df4ca620271a9460c79e11d8b6cc
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-20 02:56:47 +0000

    Added a function that renders tweets by appending to a class

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,25 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-const renderTweets = function () {
-  
+const renderTweets = function (data) {
+  let results = '';
+  for (const tweet of data) {
+    results += createTweetElement(tweet);
+  }
+  return $('#tweets-container').append(results);
 }
 
 const createTweetElement = function (data) {
   const $tweet = `
-  <article>
+      <article>
         <header>
           <div>
             <img src="${data.user.avatars}" class="user-icon">
@@ -42,9 +59,4 @@ const createTweetElement = function (data) {
   return $tweet;
 }
 
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
+renderTweets(data);
